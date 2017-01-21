4077af69f36e076b1ce906fb6b622b2a78755a7b
Marco Grießhammer <griesshammer@yamud.de>
2017-01-21 20:53:44 +0100

    NinjectModule.cs

---
+++ Common.Logging.Ninject/NinjectModule.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Common.Logging.Ninject
+{
+    public class NinjectModule : global::Ninject.Modules.NinjectModule
+    {
+        public override void Load()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
